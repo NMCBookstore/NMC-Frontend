9881fc4242cce5ee71cbe6a8495661c64125577c
culi110101 <buidinhxuanit@gmail.com>
2023-10-16 14:47:38 +0700

    update footer

--- src/component/footer/index.tsx
+++ src/component/footer/index.tsx
@@ -1,9 +1,105 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Footer: React.FunctionComponent = () => {
     return (
         <div>
-            <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+            <div className="footer bg-[#fcfcfc]">
+                <div className="lg:container mx-auto pt-10 pb-14">
+                    <div className="flex flex-row">
+                        <div className="px-3 basis-1/3">
+                            <h3 className="text-[20px] text-primary font-semibold leading-normal mb-6" >NMC Bookstore</h3>
+                            <ul className="flex flex-col gap-4">
+                                <li className="flex items-center"  >
+                                    <i className=" mr-2 text-primary text-[26px] inline-flex items-center bdx-location"></i> <p className="inline text-[16px] leading-[24px]">1 Vo Van Ngan, TP. Thu Duc</p>
+                                </li>
+                                <li className="flex items-center"  >
+                                    <i className=" mr-2 text-primary text-[22px] inline-flex items-center bdx-phone"></i> <p className="inline text-[16px] leading-[24px]">033 555 8814</p>
+                                </li>
+                                <li className="flex items-center"  >
+                                    <i className=" mr-2 text-primary text-[22px] inline-flex items-center bdx-phone"></i> <p className="inline text-[16px] leading-[24px]">012 345 6789</p>
+                                </li>
+                            </ul>
+                            <div className="mt-8">
+                                <h3 className="text-[20px] text-primary font-semibold leading-normal mb-4">Follow us</h3>
+                                <ul className="flex gap-3">
+                                    <li className="w-[48px] h-[48px] bg-[#1877F2] rounded-full flex items-center justify-center"><i className="bdx-facebook text-[white] text-[22px] inline-flex items-center"></i></li>
+                                    <li className="w-[48px] h-[48px] bg-[#1DA1F2] rounded-full flex items-center justify-center" ><i className="text-[white] text-[22px] inline-flex items-center bdx-twitter"></i></li>
+                                    <li className="w-[48px] h-[48px] bg-[#FF0000] rounded-full flex items-center justify-center" ><i className="text-[white] text-[22px] inline-flex items-center bdx-youtube"></i></li>
+                                    <li className="w-[48px] h-[48px] bg-[#F00073] rounded-full flex items-center justify-center" ><i className="text-[white] text-[22px] inline-flex items-center bdx-instagram"></i></li>
+                                    <li className="w-[48px] h-[48px] bg-[#E60023] rounded-full flex items-center justify-center" ><i className="text-[white] text-[22px] inline-flex items-center bdx-pinterest"></i></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="px-3 basis-1/4">
+                            <h3 className="text-[20px] text-primary font-semibold leading-normal mb-6" >Popular categories</h3>
+                            <ul className="flex flex-col gap-4">
+                                <li>
+                                    <Link to="Fiction">Fiction</Link>
+                                </li>
+                                <li>
+                                    <Link to="Non-Fiction">Non-Fiction</Link>
+                                </li>
+                                <li>
+                                    <Link to="Children’s">Children’s</Link>
+                                </li>
+                                <li>
+                                    <Link to="Stationery and gifts">Stationery and gifts</Link>
+
+                                </li>
+                                <li>
+                                    <Link to="Gift cards and vouchers">Gift cards and vouchers</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="px-3 basis-1/6">
+                            <h3 className="text-[20px] text-primary font-semibold leading-normal mb-6" >About us</h3>
+                            <ul className="flex flex-col gap-4">
+                                <li>
+                                    <Link to="Our-story">Our story</Link>
+                                </li>
+                                <li>
+                                    <Link to="Blog">Blog</Link>
+                                </li>
+                                <li>
+                                    <Link to="Events">Events</Link>
+                                </li>
+                                <li>
+                                    <Link to="Contact">Contact</Link>
+                                </li>
+                                <li>
+                                    <Link to="Careers">Careers</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="px-3 basis-1/4">
+                            <h3 className="text-[20px] text-primary font-semibold leading-normal mb-6" >Support</h3>
+                            <ul className="flex flex-col gap-4">
+                                <li>
+                                    <Link to="track-order">Track order</Link>
+                                </li>
+                                <li>
+                                    <Link to="delivery-methods">Delivery methods</Link>
+                                </li>
+                                <li>
+                                    <Link to="guarantee-and-returns">Guarantee and returns</Link>
+                                </li>
+                                <li>
+                                    <Link to="FAQs">FAQs</Link>
+                                </li>
+                                <li>
+                                    <Link to="terms-and-Privacy">Terms and Privacy</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-[#F0F0F0] border-t border-[#BFBFBF] border-solid">
+                <p className="lg:container mx-auto text-center py-4">© 2023 NMC All Rights Reserved</p>
+            </div>
+
+            {/* <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
                 <div className="fixed inset-0 bg-backdrop bg-opacity-75 transition-opacity"></div>
                 <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                     <div className="flex min-h-full items-end justify-center p-4 text-center items-center sm:p-0">
@@ -18,10 +114,10 @@ const Footer: React.FunctionComponent = () => {
                                                 <label className="block text-[16px] leading-normal mb-2" htmlFor="name">Name<span>*</span></label>
                                                 <input className="rounded-full border-[1px] border-[#BFBFBF] border-solid px-4 py-3 w-full" type="text" name="name" placeholder="Username" />
                                             </div>
-                                            {/* <div className="text-left mb-4">
+                                            <div className="text-left mb-4">
                                                 <label className="block text-[16px] leading-normal mb-2" htmlFor="name">Email<span>*</span></label>
                                                 <input className="rounded-full border-[1px] border-[#BFBFBF] border-solid px-4 py-3 w-full" type="text" name="email" placeholder="example@gmail.com" />
-                                            </div> */}
+                                            </div>
                                             <div className="text-left">
                                                 <label className="block text-[16px] leading-normal mb-2" htmlFor="password">Password<span>*</span></label>
                                                 <input className="rounded-full border-[1px] border-[#BFBFBF] border-solid px-4 py-3 w-full" type="text" name="password" placeholder="*****" />
@@ -46,9 +142,7 @@ const Footer: React.FunctionComponent = () => {
                         </form>
                     </div>
                 </div>
-            </div>
-
-            <h1>footer</h1>
+            </div> */}
         </div>
     );
 };
