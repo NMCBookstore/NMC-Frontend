ebd63dc7674bbc746e4c4f55ff343521be8eeed7
culi110101 <buidinhxuanit@gmail.com>
2023-10-18 15:05:31 +0700

    update hoàn thiện banner

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -9,7 +9,7 @@ const HomePage: React.FunctionComponent = () => {
     const banner = ["img", "img", "igm"];
     const settings = {
         dots: true,
-        infinite: true,
+        infinite: false,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1
@@ -17,8 +17,8 @@ const HomePage: React.FunctionComponent = () => {
     return (
         <div>
             <div className="marquee-container">
-                <div className="mx-auto">
-                    <div className="marquee px-3">
+                <div className="flex">
+                    <div className="marquee px-3 mx-auto">
                         <p>Shop all <span>◆</span> </p>
                         <p>New arrivals <span>◆</span> </p>
                         <p>Bestsellers <i className="bdx-like ml-2 text-accent inline-flex items-center"></i><span>◆</span> </p>
@@ -32,12 +32,12 @@ const HomePage: React.FunctionComponent = () => {
             </div>
             <Slider {...settings}>
                 {banner.map((item, index) => (
-                    <div className="banner bg-[#FBF4EA]">
-                        <div className="mx-auto container-nmc">
-                            <div className="w-[70%]">
-                                <p>Welcome to the NMC Bookstore</p>
-                                <h1>Your Gateway to a World of Knowledge and Imagination!</h1>
-                                <Link to="product">SHOP NOW</Link>
+                    <div className="banner bg-[#FBF4EA] pt-[84px] pb-[100px]">
+                        <div className="mx-auto container-nmc px-3">
+                            <div className="w-[60%] lg:w-[70%] flex flex-col justify-center">
+                                <p className="mb-2 text-[20px] font-semibold" >Welcome to the NMC Bookstore</p>
+                                <h1 className="lg:text-[36px] text-[56px] leading-normal font-bold mb-6" >Your Gateway to a World of Knowledge and Imagination!</h1>
+                                <Link to="product" className="flex py-3 px-6 items-center bg-orange-orange-6 w-fit rounded-full"> <i className="bdx-cart-fill inline-flex mr-2 items-center"></i> SHOP NOW</Link>
                             </div>
                             <div className="w-[30%] flex items-center">
                                 <img src={bannerImg} alt="banner" />
