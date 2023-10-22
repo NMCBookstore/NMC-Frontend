1938e33af252ceff786ad2daabc498c08d6b6efc
culi110101 <buidinhxuanit@gmail.com>
2023-10-22 18:39:07 +0700

    update ẩn hiện thanh search ở header

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from 'react-router-dom';
 import NoteNotify from "../NoteNotify";
-import {logo} from "../../assets/img";
+import { logo } from "../../assets/img";
 
 const Header: React.FunctionComponent = () => {
+    const [showSearch, setshowSearch] = useState<boolean | null>(false);
     const numberCount1: number = 10;
     const numberCount3: number = 6;
     const numberCount2: number = 2;
@@ -19,7 +20,20 @@ const Header: React.FunctionComponent = () => {
                 </div>
                 <div className='header__content flex flex-row justify-end px-[12px] sm:px-[24px] gap-[24px] grow'>
                     <div className='hidden sm:flex'>
-                        <i className="bdx-search-2 text-[20px] text-[#fff] flex items-center"></i>
+                        <i className="bdx-search-2 text-[20px] text-[#fff] flex items-center"
+                            onClick={()=>setshowSearch(!showSearch)}    
+                        ></i>
+                        <div className={`${showSearch ? "visible" : "invisible"} inset-0 w-full z-10`} aria-labelledby="modal-title" role="dialog" aria-modal="true">
+                            <div className="fixed inset-0 bg-backdrop bg-opacity-75 transition-opacity"
+                                onClick={()=>setshowSearch(!showSearch)}
+                            ></div>
+                            <div className='fixed absolute p-3 left-0 top-[76px] z-10 w-[100vw] overflow-y-auto'>
+                                <form className="flex items-end justify-center text-center sm:items-start relative">
+                                    <input className="relative w-full rounded-[16px] py-[16px] px-[24px] z-0" type="text" />
+                                    <button className="absolute top-[6px] right-[8px] bg-primary z-1 w-[44px] h-[44px] rounded-[12px] right-[18px]"><i className="bdx-search-2 text-[20px] h-full flex items-center justify-center text-[#fff]"></i></button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                     <div className="flex">
                         <Link to="/wishlist" className="flex flex-col items-center justify-center cursor-pointer hover-text-orange-orange-4-header">
