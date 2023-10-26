9767087e4192341b281d46a0055c65e129552276
Cương Nguyễn <cuongnguyen14122001@gmail.com>
2023-10-27 02:31:16 +0700

    refactor(Route): change to routing configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,35 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { useRoutes } from "react-router-dom";
 
-import './App.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import HomePage from "./page/HomePage/home"
-import NotFound from "./page/Error"
-import Header from './component/Header';
-import Cart from './page/Cart';
-import OrderInfo from "./page/Order/info"
-import OrderPayment from './page/Order/payment';
-import OrderReturn from './page/Order/return';
-import Footer from './component/Footer';
-import { store, persistor } from './app/store';
+import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import RouterConfig from "./RouterConfig";
 
-import { PersistGate } from 'redux-persist/lib/integration/react';
+const App = () => {
+  const routing = useRoutes(RouterConfig);
+  return (
+    // <Provider store={store}>
+    //     <PersistGate loading = {null} persistor={persistor}>
 
+    //     <Router>
+    //         <Header />
+    //         <Routes>
+    //             <Route path="/" element={<HomePage/>} />
+    //             <Route path="/cart" element={<Cart />} />
+    //             <Route path="/order/info" element={<OrderInfo/>} />
+    //             <Route path="/order/payment" element={<OrderPayment />} />
+    //             <Route path="/order/return" element={<OrderReturn />} />
+    //             <Route path="*" element={<NotFound />} />
+    //         </Routes>
+    //         <Footer />
 
-function App() {
-    return (
-        <Provider store={store}>
-            <PersistGate loading = {null} persistor={persistor}>
+    //     </Router>
+    //     </PersistGate>
 
-            <Router>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<HomePage/>} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/order/info" element={<OrderInfo/>} />
-                    <Route path="/order/payment" element={<OrderPayment />} />
-                    <Route path="/order/return" element={<OrderReturn />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-                <Footer />
-
-            </Router>
-            </PersistGate>
-
-        </Provider>
-    );
-}
+    // </Provider>
+    <> {routing}</>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './assets/styles/main.scss';
-import './assets/vendor/css/bdxfont.css'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./assets/styles/main.scss";
+import "./assets/vendor/css/bdxfont.css";
+import { Provider } from "react-redux";
+import { store, persistor } from "./app/store";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Suspense
+          fallback={
+            <div style={{ display: "flex", justifyContent: "center" }}></div>
+          }
+        >
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
+        </Suspense>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
