e2617428011fd0d9256c03e92133064d10e5c85d
culi110101 <buidinhxuanit@gmail.com>
2023-11-23 20:38:52 +0700

    init article.detail

--- src/RouterConfig.tsx
+++ src/RouterConfig.tsx
@@ -9,12 +9,16 @@ const OrderInfo = lazy(() => import("./page/Order/info"));
 const OrderPayment = lazy(() => import("./page/Order/payment"));
 const OrderReturn = lazy(() => import("./page/Order/return"));
 const Cart = lazy(() => import("./page/Cart"));
+const CartDemo = lazy(() => import("./page/Cart"));
+const ProductDetail = lazy(() => import("./page/Product/detail"));
+const ArticleDetail = lazy(() => import("./page/Article/detail"));
+const ProductList = lazy(() => import("./page/Product"));
 
 /**** Protected User Routes *****/
 const RequireAuth = lazy(() => import("./features/auth/RequireAuth"));
 
 /**** 404 page *****/
-const NotFound = lazy(() => import("./page/error"));
+const NotFound = lazy(() => import("./page/Error"));
 
 const HomePage = lazy(() => import("./page/HomePage/home"));
 
@@ -27,6 +31,10 @@ const RouterConfig: RouteObject[] = [
       { path: "/order/info", element: <OrderInfo /> },
       { path: "/order/payment", element: <OrderPayment /> },
       { path: "/order/return", element: <OrderReturn /> },
+      { path: "/product/detail", element: <ProductDetail /> },
+      { path: "/product/list", element: <ProductList /> },
+      { path: "/article/detail", element: <ArticleDetail /> },
+      { path: "/cartdemo", element: <CartDemo /> },
       { path: "*", element: <NotFound /> },
     ],
   },
