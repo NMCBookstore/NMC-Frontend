75b830bc852c0a2230a8bd8b7fe39060025b6980
culi110101 <buidinhxuanit@gmail.com>
2023-12-27 10:34:34 +0700

    remove .next

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"../components/chart.js -> react-apexcharts\":{\"id\":\"../components/chart.js -> react-apexcharts\",\"files\":[\"static/chunks/node_modules_react-apexcharts_dist_react-apexcharts_min_js.js\"]}}"
